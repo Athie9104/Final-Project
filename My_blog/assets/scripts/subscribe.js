aba57960f94e6a778ae46ea00255678798c3a074
Athie9104 <athenkosigongotha7@gmail.com>
2021-03-04 16:18:13 +0200

    Additions

---
+++ My_blog/assets/scripts/subscribe.js
@@ -0,0 +1,29 @@
+
+//subscription
+function addSubscribers() {
+    const list = document.getElementById('subscribe')
+    const inputs = list.getElementsByTagName("input");
+    console.log(inputs);
+    const user = {
+        name: inputs[0].value,
+        lastname: inputs[1].value,
+        email: inputs[2].value,
+        cell: inputs[3].value,
+    }
+
+    console.log(user);
+
+    fetch("http://127.0.0.1:5000/add-new-subscriber/", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+        })
+        .then((response)=> response.json())
+        .then((json)=> {
+            console.log(json);
+            alert("You have successfully subscribed");
+            document.getElementById("subscribe").reset();
+        });  
+}
